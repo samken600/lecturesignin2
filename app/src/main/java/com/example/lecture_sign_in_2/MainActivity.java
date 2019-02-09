42a92cb37251913bcde9bd6df2d01903d6603dd0
redscorp98 <37073183+redscorp98@users.noreply.github.com>
2019-02-09 17:44:35 +0000

    Jacoob and Em's test changes

--- app/src/main/java/com/example/lecture_sign_in_2/MainActivity.java
+++ app/src/main/java/com/example/lecture_sign_in_2/MainActivity.java
@@ -1,32 +1,99 @@
 package com.example.lecture_sign_in_2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd;
+    private Button btnViewData;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnViewData = (Button) findViewById(R.id.btnView);
+        mDatabaseHelper = new DatabaseHelper(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        Button button = (Button) findViewById(R.id.button);
+
+        btnAdd.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String newEntry = editText.getText().toString();
+                if (editText.length() !=0){
+                    AddData(newEntry);
+                    editText.setText("");
+                } else {
+                    toastMessage("You must put something in the text field!");
+                }
+            }
+        });
+
+        btnViewData.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ListDataActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+
+        button.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+
+                goToSecondActivity();
+
+            }
+
+        });
     }
+
+    public void AddData(String newEntry){
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if(insertData){
+            toastMessage("Data Successfully Inserted!");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +115,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void goToSecondActivity() {
+
+        Intent intent = new Intent(this, page2.class);
+
+        startActivity(intent);
+
     }
 }
